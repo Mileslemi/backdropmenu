f0ef7e62da7c675d1d16d7cdee77907112d46f60
Miles Lemi <mileslemi@gmail.com>
2023-05-04 22:00:14 +0300

    found a workaround. Instead of using animated icons, I used two icons. Now works good.

--- lib/backdroppage.dart
+++ lib/backdroppage.dart
@@ -35,13 +35,13 @@ class _BackdropPageState extends State<BackDropPage>
 
   Animation<RelativeRect> _getPanelAnimation(BoxConstraints constraints) {
     final double height = constraints.biggest.height;
-    final double top = height - arrowPanelHeight;
-    const double bottom = -arrowPanelHeight;
+    // final double top = height - arrowPanelHeight;
+    // const double bottom = -arrowPanelHeight;
     return RelativeRectTween(
-      begin: RelativeRect.fromLTRB(0.0, top, 0.0, bottom),
+      begin: RelativeRect.fromLTRB(0.0, height, 0.0, 0),
       end: const RelativeRect.fromLTRB(0.0, 0.0, 0.0, 0.0),
     ).animate(
-        CurvedAnimation(parent: _controller, curve: Curves.fastOutSlowIn));
+        CurvedAnimation(parent: _controller, curve: Curves.linearToEaseOut));
   }
 
   Widget _buildStack(BuildContext context, BoxConstraints constraints) {
@@ -61,11 +61,28 @@ class _BackdropPageState extends State<BackDropPage>
               height: MediaQuery.of(context).size.height,
               width: double.infinity,
               color: Colors.black,
-              child: const Center(
-                child: Text(
-                  "Menu Here",
-                  style: TextStyle(color: Colors.white),
-                ),
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  const Center(
+                    child: Text(
+                      "Menu Here",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 2,
+                    child: IconButton(
+                      onPressed: () {
+                        _controller.fling(velocity: 1.0);
+                      },
+                      icon: const Icon(
+                        Icons.arrow_circle_up_outlined,
+                        color: Colors.white,
+                      ),
+                    ),
+                  )
+                ],
               ),
             ),
           ),
@@ -88,13 +105,9 @@ class _BackdropPageState extends State<BackDropPage>
                   child: Center(
                     child: IconButton(
                       onPressed: () {
-                        _controller.fling(
-                            velocity: _isPanelVisible ? -1.0 : 1.0);
+                        _controller.fling(velocity: -1.0);
                       },
-                      icon: AnimatedIcon(
-                        icon: AnimatedIcons.menu_arrow,
-                        progress: _controller.view,
-                      ),
+                      icon: const Icon(Icons.arrow_circle_down_outlined),
                     ),
                   ),
                 ),
